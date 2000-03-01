900713c9234d1ccedc442ff42a33892345b984a0
d3august <d3august>
2000-03-01 00:57:37 +0000

    Ordnat typerna lite s} de blir l{ttare att l{sa.

--- ogle/include/common.h
+++ ogle/include/common.h
@@ -9,16 +9,16 @@ enum {
 
 /* "There's len bytes of data for you in your file at this offset." */
 struct off_len_packet {
-	               uint32_t cmd;
-                       uint32_t off;
-                       uint32_t len;
+//	               uint32_t type;
+                       uint32_t offset;
+                       uint32_t length;
                       };
 
 /* "mmap this file now; off_len_packets from now on will be relative
  * this file." */
 struct load_file_packet {
-                         uint32_t cmd;
-                         uint32_t len;
+//                        uint32_t type;
+                         uint32_t length;
                          char     filename[200];   // HACK ALERT!
                         };
 
