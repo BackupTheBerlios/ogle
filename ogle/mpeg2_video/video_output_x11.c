707fee9e96c5f64b72fccccc0e4078c5cdc497a0
d95hjort <d95hjort>
2000-07-27 21:18:15 +0000

    Add support for scaling the decoded picture befor displaying it. New keybord shortcuts are '1' '2' and '3' to scale to 100%, 200% and 300% of the original size respectively. 's' and then 1-4 is mapped to different scaling modes (Nearest, bilinear, bicubic and bicubic2).

---
+++ ogle/mpeg2_video/video_output_x11.c
@@ -0,0 +1,634 @@
+/* SKROMPF - A video player
+ * Copyright (C) 2000 Björn Englund, Håkan Hjort
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#ifdef HAVE_MLIB
+#include <mlib_types.h>
+#include <mlib_status.h>
+#include <mlib_sys.h>
+#include <mlib_video.h>
+#include <mlib_algebra.h>
+#include <mlib_image.h>
+#else
+#ifdef HAVE_MMX
+#include "mmx.h"
+#endif
+#include "c_mlib.h"
+#endif
+
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/extensions/XShm.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+#include <glib.h>
+
+#include "../include/common.h"
+#include "video_types.h"
+#include "yuv2rgb.h"
+#include "screenshot.h"
+
+
+typedef struct {
+  Window win;
+  unsigned char *data;
+  XImage *ximage;
+  yuv_image_t *image;
+  int grid;
+  int color_grid;
+  macroblock_t *mbs;
+} debug_win;
+
+static debug_win windows[1];
+
+
+static XVisualInfo vinfo;
+static XShmSegmentInfo shm_info;
+static Display *mydisplay;
+static Window window_stat;
+static GC mygc;
+static GC statgc;
+static char *title = "Mjölner";
+static int color_depth, pixel_stride, mode;
+
+
+extern unsigned int debug;
+static int show_window[1] = {1};
+static int run = 0;
+static int screenshot = 0;
+static int scaled_image_width;
+static int scaled_image_height;
+static int scalemode = MLIB_BILINEAR;
+
+
+
+void exit_program(int);
+
+void draw_win(debug_win *dwin);
+
+
+void display_init(int padded_width, int padded_height,
+		  int horizontal_size, int vertical_size)
+{
+  int screen;
+  
+  XSizeHints hint;
+  XEvent xev;
+  XGCValues xgcv;
+  Colormap theCmap;
+  XWindowAttributes attribs;
+  XSetWindowAttributes xswa;
+  unsigned long xswamask;
+
+
+  mydisplay = XOpenDisplay(NULL);
+
+  if (mydisplay == NULL)
+    fprintf(stderr,"Can not open display\n");
+
+  /* Check for availability of shared memory */
+  if (!XShmQueryExtension(mydisplay)) {
+    fprintf(stderr, "No shared memory available!\n");
+    exit(1);
+  }
+
+  screen = DefaultScreen(mydisplay);
+
+  hint.x = 0;
+  hint.y = 0;
+  hint.width = horizontal_size;
+  hint.height = vertical_size;
+  hint.flags = PPosition | PSize;
+
+  /* Make the window */
+  XGetWindowAttributes(mydisplay, DefaultRootWindow(mydisplay), &attribs);
+  color_depth = attribs.depth;
+  if (color_depth != 15 && color_depth != 16 && 
+      color_depth != 24 && color_depth != 32) {
+    fprintf(stderr,"Only 15,16,24, and 32bpp supported. Trying 24bpp!\n");
+    color_depth = 24;
+  }
+  
+  XMatchVisualInfo(mydisplay,screen,color_depth,TrueColor,&vinfo);
+  printf("visual id is  %lx\n",vinfo.visualid);
+
+  theCmap   = XCreateColormap(mydisplay, RootWindow(mydisplay,screen), 
+			      vinfo.visual, AllocNone);
+
+  xswa.background_pixel = 0;
+  xswa.border_pixel     = 1;
+  xswa.colormap         = theCmap;
+  xswamask = CWBackPixel | CWBorderPixel | CWColormap;
+
+  windows[0].win = XCreateWindow(mydisplay, RootWindow(mydisplay,screen),
+				 hint.x, hint.y, hint.width, hint.height, 
+				 0, color_depth, CopyFromParent, vinfo.visual, 
+				 xswamask, &xswa);
+
+  window_stat = XCreateSimpleWindow(mydisplay, RootWindow(mydisplay,screen),
+				    hint.x, hint.y, 200, 200, 0,
+				    0, 0);
+
+  XSelectInput(mydisplay, windows[0].win, StructureNotifyMask | KeyPressMask 
+	       | ButtonPressMask | ExposureMask);
+  XSelectInput(mydisplay, window_stat, StructureNotifyMask | KeyPressMask 
+	       | ButtonPressMask | ExposureMask);
+
+  /* Tell other applications about this window */
+  XSetStandardProperties(mydisplay, windows[0].win, 
+			 title, title, None, NULL, 0, &hint);
+  XSetStandardProperties(mydisplay, window_stat, 
+			 "stat", "stat", None, NULL, 0, &hint);
+
+  /* Map window. */
+  XMapWindow(mydisplay, window_stat);
+  XMapWindow(mydisplay, windows[0].win);
+  
+  
+  // This doesn't work correctly
+  /* Wait for map. */
+  do {
+    XNextEvent(mydisplay, &xev);
+  }
+  while (xev.type != MapNotify || xev.xmap.event != windows[0].win);
+  
+  //   XSelectInput(mydisplay, mywindow, NoEventMask);
+
+  XFlush(mydisplay);
+  XSync(mydisplay, False);
+   
+  /* Create the colormaps. */   
+  mygc = XCreateGC(mydisplay, windows[0].win, 0L, &xgcv);
+  statgc = XCreateGC(mydisplay, window_stat, 0L, &xgcv);
+   
+  /* Create shared memory image */
+  windows[0].ximage = XShmCreateImage(mydisplay, vinfo.visual, color_depth,
+				      ZPixmap, NULL, &shm_info,
+				      padded_width,
+				      padded_height);
+  
+  if (windows[0].ximage == NULL) {
+    fprintf(stderr, "Shared memory: couldn't create Shm image\n");
+    goto shmemerror;
+  }
+  
+  /* Get a shared memory segment */
+  shm_info.shmid = shmget(IPC_PRIVATE,
+			  windows[0].ximage->bytes_per_line * 
+			  windows[0].ximage->height, 
+			  IPC_CREAT | 0777);
+  
+  if (shm_info.shmid < 0) {
+    fprintf(stderr, "Shared memory: Couldn't get segment\n");
+    goto shmemerror;
+  }
+  
+  /* Attach shared memory segment */
+  shm_info.shmaddr = (char *) shmat(shm_info.shmid, 0, 0);
+  if (shm_info.shmaddr == ((char *) -1)) {
+    fprintf(stderr, "Shared memory: Couldn't attach segment\n");
+    goto shmemerror;
+  }
+  
+  windows[0].ximage->data = shm_info.shmaddr;
+  shm_info.readOnly = False;
+  XShmAttach(mydisplay, &shm_info);
+  XSync(mydisplay, 0);
+
+  windows[0].data = windows[0].ximage->data;
+  
+  pixel_stride = windows[0].ximage->bits_per_pixel;
+  // If we have blue in the lowest bit then obviously RGB 
+  mode = ((windows[0].ximage->blue_mask & 0x01) != 0) ? 1 : 2;
+  /*
+#ifdef WORDS_BIGENDIAN 
+  if (windows[0].ximage->byte_order != MSBFirst)
+#else
+  if (windows[0].ximage->byte_order != LSBFirst) 
+#endif
+  {
+    fprintf( stderr, "No support for non-native XImage byte order!\n" );
+    exit(1);
+  }
+  */
+  yuv2rgb_init(pixel_stride, mode);
+  
+  /* Scale init. */
+  scaled_image_width = horizontal_size;
+  scaled_image_height = vertical_size;
+  
+  return;
+  
+ shmemerror:
+  // exit_program();
+  exit(1);
+
+}
+
+void display_chage_size(int new_width, int new_height) {
+  XSizeHints hint;
+  int padded_width = windows[0].image->padded_width;
+  int padded_height = windows[0].image->padded_height;
+  
+  // Check to not 'reallocate' if the size is the same or less than 
+  // minimum size...
+  if( (new_width < padded_width && new_height < padded_height) ||
+      (scaled_image_width == new_width && scaled_image_height == new_height) ){
+    scaled_image_width = new_width;
+    scaled_image_height = new_height;
+    return;
+  }
+  
+  /* Save the new size so we know what to scale to. */
+  scaled_image_width = new_width;
+  scaled_image_height = new_height;
+  
+  /* Stop events temporarily, while creating new display_image */
+  XSelectInput(mydisplay, windows[0].win, NoEventMask);
+  
+  XSync(mydisplay,True);
+  
+  /* Destroy old display */
+  XShmDetach(mydisplay, &shm_info);
+  XDestroyImage(windows[0].ximage);
+  shmdt(shm_info.shmaddr);
+  if(shm_info.shmaddr == ((char *) -1)) {
+    fprintf(stderr, "Shared memory: Couldn't detache segment\n");
+    exit(-10);
+  }
+  shmctl(shm_info.shmid, IPC_RMID, 0);
+  if(shm_info.shmid == -1) {
+    fprintf(stderr, "Shared memory: Couldn't rm segment\n");
+    exit(-10);
+  }
+  XSync(mydisplay, True);
+  
+  memset( &shm_info, 0, sizeof( XShmSegmentInfo ) );
+  
+  /* Create new display */
+  windows[0].ximage = XShmCreateImage(mydisplay, vinfo.visual, 
+				      color_depth, ZPixmap, 
+				      NULL, &shm_info,
+				      scaled_image_width,
+				      scaled_image_height);
+  
+  if(windows[0].ximage == NULL) {
+    fprintf(stderr, "Shared memory: couldn't create Shm image\n");
+    exit(-10);
+  }
+  
+  /* Get a shared memory segment */
+  shm_info.shmid = shmget(IPC_PRIVATE,
+			  windows[0].ximage->bytes_per_line * 
+			  windows[0].ximage->height, 
+			  IPC_CREAT | 0777);
+  
+  if(shm_info.shmid < 0) {
+    fprintf(stderr, "Shared memory: Couldn't get segment\n");
+    exit(-10);
+  }
+  
+  /* Attach shared memory segment */
+  shm_info.shmaddr = (char *) shmat(shm_info.shmid, 0, 0);
+  if(shm_info.shmaddr == ((char *) -1)) {
+    fprintf(stderr, "Shared memory: Couldn't attach segment\n");
+    exit(-10);
+  }
+  
+  windows[0].ximage->data = shm_info.shmaddr;
+  windows[0].data = windows[0].ximage->data;
+  
+  shm_info.readOnly = False;
+  XShmAttach(mydisplay, &shm_info);
+  
+  XSync(mydisplay, 0);
+  
+  
+  /* Force a change of the widow size. */
+  XResizeWindow(mydisplay, windows[0].win, 
+		scaled_image_width, scaled_image_height);
+  
+  /* Turn on events */
+  XSelectInput(mydisplay, windows[0].win, StructureNotifyMask 
+	       | KeyPressMask | ButtonPressMask | ExposureMask);
+}
+
+
+void display_exit(void) 
+{
+  // Need to add some test to se if we can detatch/free/destroy things
+  XShmDetach(mydisplay, &shm_info);
+  XDestroyImage(windows[0].ximage);
+  shmdt(shm_info.shmaddr);
+  shmctl(shm_info.shmid, IPC_RMID, 0);
+}
+
+
+
+ 
+void add_grid(unsigned char *data, XImage *ximg)
+{
+  int m,n;
+  
+  for(m = 0; m < ximg->height; m++) {
+    if(m%16 == 0) {
+      for(n = 0; n < ximg->width*4; n+=4) {
+	data[m*ximg->width*4+n+1] = 127;
+	data[m*ximg->width*4+n+2] = 127;
+	data[m*ximg->width*4+n+3] = 127;
+      }
+    } else {
+      for(n = 0; n < ximg->width*4; n+=16*4) {
+	data[m*ximg->width*4+n+1] = 127;
+	data[m*ximg->width*4+n+2] = 127;
+	data[m*ximg->width*4+n+3] = 127;
+      }
+    }
+  }
+}
+
+
+
+void add_2_box_sides(unsigned char *data,
+		     unsigned char r, unsigned char g, unsigned char b,
+		     XImage *ximg)
+{
+  int n;
+  
+  for(n = 0; n < 16*4; n+=4) {
+    data[n+1] = b;
+    data[n+2] = g;
+    data[n+3] = r;
+  }
+  
+  for(n = 0; n < ximg->width*4*16; n+=ximg->width*4) {
+    data[n+1] = b;
+    data[n+2] = g;
+    data[n+3] = r;
+  }
+  
+  return;
+}
+
+
+/* This assumes a 720x480 image */
+void add_color_grid(debug_win *win)
+{
+  int m;
+  
+  for(m = 0; m < 30*45; m++) {
+    if(win->mbs[m].skipped) {
+
+      add_2_box_sides(&(win->data[m/45*win->ximage->width*4*16+(m%45)*16*4]),
+		      150, 150, 150, win->ximage);
+      
+    } else if(win->mbs[m].modes.macroblock_intra) {
+
+      add_2_box_sides(&(win->data[m/45*win->ximage->width*4*16+(m%45)*16*4]),
+		      0, 255, 255, win->ximage);
+      
+    } else if(win->mbs[m].modes.macroblock_motion_forward &&
+	      win->mbs[m].modes.macroblock_motion_backward) {
+      
+      add_2_box_sides(&(win->data[m/45*win->ximage->width*4*16+(m%45)*16*4]),
+		      255, 0, 0, win->ximage);
+      
+    } else if(win->mbs[m].modes.macroblock_motion_forward) {
+      
+      add_2_box_sides(&(win->data[m/45*win->ximage->width*4*16+(m%45)*16*4]),
+		      255, 255, 0, win->ximage);
+
+    } else if(win->mbs[m].modes.macroblock_motion_backward) {
+      
+      add_2_box_sides(&(win->data[m/45*win->ximage->width*4*16+(m%45)*16*4]),
+		      0, 0, 255, win->ximage);
+    }
+  }
+}
+
+
+void display(yuv_image_t *current_image)
+{
+  XEvent ev;
+  int nextframe = 0;  
+  
+  windows[0].image = current_image;
+  
+  if(show_window[0]) {
+    draw_win(&windows[0]);
+  }
+  
+  while(!nextframe) {
+
+    if(run) {
+      nextframe = 1;
+      if(XCheckMaskEvent(mydisplay, 0xFFFFFFFF, &ev) == False) {
+	continue;
+      }
+    } else {
+      XNextEvent(mydisplay, &ev);
+    }
+
+    switch(ev.type) {
+    case Expose:
+      if(windows[0].win == ev.xexpose.window)
+	if(show_window[0])
+	  draw_win(&(windows[0]));
+      break;
+    case ConfigureNotify:
+      if(ev.xconfigure.window == windows[0].win) {
+	display_chage_size( ev.xconfigure.width, ev.xconfigure.height );
+      }
+      break;    
+    case ButtonPress:
+      switch(ev.xbutton.button) {
+      case 0x1:
+	break;
+      case 0x2:
+	break;
+      case 0x3:
+	break;
+      }
+      break;
+    case KeyPress:
+      {
+	char buff[2];
+	static int debug_change = 0;
+	static int scalemode_change = 0;
+
+	XLookupString(&(ev.xkey), buff, 2, NULL, NULL);
+	buff[1] = '\0';
+	switch(buff[0]) {
+	case 'n':
+	  nextframe = 1;
+	  break;
+	case 'd':
+	  debug_change = 1;
+	  break;
+	case 'w':
+	  show_window[0] = !show_window[0];
+	  break;
+	case 'r':
+	  run = !run;
+	  nextframe = 1;
+	  break;
+	case 'p':
+	  screenshot = 1;
+	  draw_win(&(windows[0]));
+	  break;
+	case 'q':
+	  display_exit();
+	  exit_program(0);
+	  break;
+	case 's':
+	  scalemode_change = 1;
+	  break;
+	case '1':
+	  if(!debug_change && !scalemode_change) {
+	    display_chage_size( windows[0].image->horizontal_size, 
+				windows[0].image->vertical_size);
+	  }
+	  break;
+	case '2':
+	  if(!debug_change && !scalemode_change) {
+	    display_chage_size( windows[0].image->horizontal_size * 2, 
+				windows[0].image->vertical_size * 2);
+	  }
+	  break;
+	case '3':
+	  if(!debug_change && !scalemode_change) {
+	    display_chage_size( windows[0].image->horizontal_size * 3,
+				windows[0].image->vertical_size * 3);
+	  }
+	  break;
+	default:
+	  break;
+	}
+	if(debug_change && buff[0] != 'd') {
+	  debug = atoi(&buff[0]);
+	  debug_change = 0;
+	}
+	if(scalemode_change && buff[0] != 's') {
+	  switch(atoi(&buff[0])) {
+	  case 1:
+	    scalemode = MLIB_NEAREST;
+	    break;
+	  case 2:
+	    scalemode = MLIB_BILINEAR;
+	    break;
+	  case 3:
+	    scalemode = MLIB_BICUBIC;
+	    break;
+	  case 4:
+	    scalemode = MLIB_BICUBIC2;
+	    break;
+	  }
+	  scalemode_change = 0;	  
+	}
+      }
+      break;
+    default:
+      break;
+    }
+  }
+
+  return;
+}
+
+
+void draw_win(debug_win *dwin)
+{
+  mlib_image *mimage_s;
+  mlib_image *mimage_d;
+  
+  /* Put the decode rgb data at the end of the data segment. */
+  char *address = dwin->data;
+  
+  int dest_size = dwin->ximage->bytes_per_line * dwin->ximage->height;
+  int sorce_size = dwin->image->padded_width*(pixel_stride/8) * dwin->image->padded_height;
+  
+  int offs = dest_size - sorce_size - 4096;
+  if( offs > 0 )
+    address += offs;
+    
+  // Because mlib_YUV* reads outofbounds we need to make sure that the end
+  // of the picture isn't on the pageboundary for in the last page allocated
+  // There is also something strange with the mlib_ImageZoom in nearest mode.
+			      
+  /* We must some how guarantee that the ximage isn't used by X11. 
+     Rigth now it's done by the XSync call at the bottom... */
+  
+  yuv2rgb(address, dwin->image->y, dwin->image->u, dwin->image->v,
+	  dwin->image->padded_width, 
+	  dwin->image->padded_height, 
+	  dwin->image->padded_width*(pixel_stride/8),
+	  dwin->image->padded_width, dwin->image->padded_width/2 );
+  
+  if(dwin->grid) {
+    add_grid(dwin->data, dwin->ximage);
+  }
+  
+  if( (scaled_image_width != dwin->image->horizontal_size) ||
+      (scaled_image_height != dwin->image->vertical_size )) {
+    /*
+    mylib_VideoScaleABGR( dwin->data,
+			  address,
+			  scaled_image_width,
+			  scaled_image_height,
+			  dwin->image->horizontal_size,
+			  dwin->image->vertical_size,
+			  MAX(dwin->image->padded_width, scaled_image_width)
+			  * (pixel_stride/8),
+			  dwin->image->padded_width*(pixel_stride/8));
+    */
+    mimage_d 
+      = mlib_ImageCreateStruct(MLIB_BYTE, 4,
+			       scaled_image_width, scaled_image_height,
+			       dwin->ximage->bytes_per_line, dwin->data);
+  
+    mimage_s = mlib_ImageCreateStruct(MLIB_BYTE, 4, 
+				      dwin->image->horizontal_size, 
+				      dwin->image->vertical_size,
+				      dwin->image->padded_width*4, address);
+    
+    mlib_ImageZoom (mimage_d, mimage_s,
+		    (double)scaled_image_width/(double)dwin->image->horizontal_size, 
+		    (double)scaled_image_height/(double)dwin->image->vertical_size,
+		    scalemode, MLIB_EDGE_DST_FILL_ZERO);
+  
+    mlib_ImageDelete(mimage_s);
+    mlib_ImageDelete(mimage_d);
+  }
+  
+  if(screenshot) {
+    screenshot=0;
+    screenshot_jpg(dwin->data, dwin->ximage);
+  }
+  
+  XShmPutImage(mydisplay, dwin->win, mygc, dwin->ximage, 0, 0, 0, 0, 
+	       scaled_image_width, scaled_image_height, 1);
+
+  XSync(mydisplay, False);
+  return;
+}
+
+
