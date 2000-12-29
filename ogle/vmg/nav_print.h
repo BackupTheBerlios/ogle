7b312b7e6b2e858b6e06a78b67548205ee0c30bd
d95hjort <d95hjort>
2000-12-29 22:05:36 +0000

    *** empty log message ***

---
+++ ogle/vmg/nav_print.h
@@ -0,0 +1,5 @@
+
+
+void print_pci_packet (FILE *out, pci_t *pci);
+void print_dsi_packet (FILE *out, dsi_t *dsi);
+
---
+++ ogle/vmg/nav_read.h
@@ -0,0 +1,3 @@
+
+void read_pci_packet (pci_t *pci, FILE *out, buffer_t *buffer);
+void read_dsi_packet (dsi_t *dsi, FILE *out, buffer_t *buffer);
