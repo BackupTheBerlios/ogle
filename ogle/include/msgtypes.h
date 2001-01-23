4fefd1e5320777220497f4464cd3bc18d5c7b2bd
d4bjorn <d4bjorn>
2001-01-23 13:55:17 +0000

    made changes to the queue system

--- ogle/include/msgtypes.h
+++ ogle/include/msgtypes.h
@@ -28,15 +28,26 @@ typedef enum {
   CMD_DEMUX_STREAM_BUFFER = 7,
   CMD_DECODE_STREAM_BUFFER = 8,
   CMD_CTRL_DATA = 9,
-  CMD_CTRL_CMD = 10
+  CMD_CTRL_CMD = 10,
+  CMD_DECODE_NEW_OUTPUT = 11,
+  CMD_DECODE_OUTPUT_BUFFER = 12,
+  CMD_OUTPUT_BUFFER = 13
 } cmdtype_t;
 
 typedef enum {
   CTRLCMD_NONE = 0,
   CTRLCMD_STOP = 1,
   CTRLCMD_PLAY = 2,
-  CTRLCMD_PLAY_FROM = 3
+  CTRLCMD_PLAY_FROM = 3,
+  CTRLCMD_PLAY_TO = 4,
+  CTRLCMD_PLAY_FROM_TO = 5
 } ctrlcmd_t;
+
+typedef enum {
+  OUTPUT_NONE = 0,
+  OUTPUT_VIDEO_YUV = 1,
+  OUTPUT_SPU = 2
+} output_t;
 
 typedef struct {
   char file[PATH_MAX+1];
@@ -63,10 +74,23 @@ typedef struct {
 } cmd_new_stream_t;
 
 typedef struct {
+  output_t type;  
+  int data_buf_size;
+  int nr_of_elems;    // nr of elements in q
+} cmd_new_output_t;
+
+typedef struct {
   uint8_t stream_id;
   uint8_t subtype;
   int q_shmid;
 } cmd_stream_buffer_t;
+
+typedef struct {
+  int data_shmid;
+  int data_size;
+  int q_shmid;
+} cmd_output_buffer_t;
+
 
 typedef struct {
   int shmid;
@@ -76,9 +100,8 @@ typedef struct {
 
 typedef struct {
   ctrlcmd_t ctrlcmd;
-  union {
-    int from_time;
-  } args;
+  int off_from;
+  int off_to;
 } cmd_ctrl_cmd_t;
 
 typedef struct {
@@ -92,6 +115,8 @@ typedef struct {
     cmd_stream_buffer_t stream_buffer;
     cmd_ctrl_data_t ctrl_data;
     cmd_ctrl_cmd_t ctrl_cmd;
+    cmd_new_output_t new_output;
+    cmd_output_buffer_t output_buffer;
   } cmd;
 } cmd_t;
 
--- ogle/include/queue.h
+++ ogle/include/queue.h
@@ -18,13 +18,41 @@ typedef struct {
   int scr_nr;
   int off;
   int len;
+  char *q_addr;
+} data_elem_t;
+
+typedef struct {
+  uint64_t PTS;
+  uint64_t DTS;
+  uint64_t SCR_base;
+  uint16_t SCR_ext;
+  int displayed;
+  int is_reference;
+  uint8_t PTS_DTS_flags;
+  uint8_t SCR_flags;
+  int scr_nr;
+  yuv_image_t picture;
+  int picture_off;
+  int picture_len;
+  char *q_addr;
+} picture_data_elem_t;
+
+typedef struct {
+  int shmid;
+  int nr_of_dataelems;
+  int write_nr;
+  int read_nr;
+} data_buf_head_t;
+
+typedef struct {
+  int data_elem_index;
 } q_elem_t;
 
 
 typedef struct {
   sem_t bufs_full;
   sem_t bufs_empty;
-  int buf_shmid;
+  int data_buf_shmid;
   int nr_of_qelems;
   int write_nr;
   int read_nr;
