48db6853c9e57c04e41e651248607a57bbaf4e55
d4bjorn <d4bjorn>
2001-10-25 17:10:19 +0000

    added authors

--- ogle/libao/audio_out_oss.c
+++ ogle/libao/audio_out_oss.c
@@ -19,6 +19,9 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+/* Simple sample rate conversion by Marc Espie */
+/* Modified slightly by Björn Englund */
+
 /* #include "config.h" */
 
 #ifdef LIBAO_OSS
