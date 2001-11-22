77d3a3e88b4a4f22ce5b1b8f9fbcc21745ff27d8
d95hjort <d95hjort>
2001-11-22 22:27:51 +0000

    Don't run autoheader.

--- ogle/autogen.sh
+++ ogle/autogen.sh
@@ -3,7 +3,7 @@ set -x
 
 aclocal
 libtoolize --copy --automake
-autoheader
+#autoheader
 #add --include-deps if you want to bootstrap with any other compiler than gcc
 #automake --copy --add-missing --include-deps
 automake --copy --add-missing
