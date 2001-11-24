ca04a3c7f07ff287c2cdd197f6e7b1cee38b0bd1
d4bjorn <d4bjorn>
2001-11-24 00:17:20 +0000

    added FATAL, ERROR, WARNING, NOTE, DNOTE, print macros

--- ogle/include/debug_print.h
+++ ogle/include/debug_print.h
@@ -18,7 +18,22 @@ int debug_indent_level;
 #endif
 
 
-#if __GNUC__ < 2 || (__GNUC__ == 2 && __GNUC_MINOR__ < 95)
+#if defined(__GNUC__) && ( __GNUC__ < 2 || (__GNUC__ == 2 && __GNUC_MINOR__ < 95))
+
+#define FATAL(str, args...) \
+fprintf(stderr, "FATAL[%s]: " str, program_name, ## args)
+
+#define ERROR(str, args...) \
+fprintf(stderr, "ERROR[%s]: " str, program_name, ## args)
+
+#define WARNING(str, ...) \
+fprintf(stderr, "WARNING[%s]: " str, program_name, ## args)
+
+#define NOTE(str, args...) \
+fprintf(stderr, "Note[%s]: " str, program_name, ## args)
+
+#define DNOTE(str, args...) \
+fprintf(stderr, "Debug[%s]: " str, program_name, ## args)
 
 #ifdef DEBUG
 
@@ -44,6 +59,25 @@ if(debug >= level) \
 #endif /* DEBUG */
 
 #else /* __GNUC__ < 2 */
+
+/* TODO: these defines are not portable */
+
+#define FATAL(format, ...) \
+fprintf(stderr, "FATAL[%s]: " format, program_name , ##__VA_ARGS__)
+
+#define ERROR(format, ...) \
+fprintf(stderr, "ERROR[%s]: " format, program_name , ##__VA_ARGS__)
+
+#define WARNING(format, ...) \
+fprintf(stderr, "WARNING[%s]: " format, program_name , ##__VA_ARGS__)
+
+#define NOTE(format, ...) \
+fprintf(stderr, "Note[%s]: " format, program_name , ##__VA_ARGS__)
+
+#define DNOTE(format, ...) \
+fprintf(stderr, "Debug[%s]: " format, program_name , ##__VA_ARGS__)
+
+
 
 #ifdef DEBUG
 
