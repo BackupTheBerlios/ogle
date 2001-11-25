23491ed9a3d405e9dda18cae1da1382d63f438ad
d95mback <d95mback>
2001-11-25 17:12:42 +0000

    no more gnome macros

--- ogle_gui/autogen.sh
+++ ogle_gui/autogen.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
-# Run this to generate all the initial makefiles, etc.
+set -x
 
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
-
-PKG_NAME="the package."
-
-(test -f $srcdir/configure.in) || {
-    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level directory"
-    exit 1
-}
-
-. $srcdir/macros/autogen.sh
+gettextize
+aclocal
+libtoolize --copy --automake
+automake --copy --add-missing
+autoconf
+autoheader
+rm -f config.cache
