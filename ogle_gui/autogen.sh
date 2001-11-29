deadbd94bc51cb8109921e1b46a0283987d8aa01
d95mback <d95mback>
2001-11-29 22:34:15 +0000

    New way to generate libglade strings

--- ogle_gui/autogen.sh
+++ ogle_gui/autogen.sh
@@ -2,16 +2,6 @@
 set -x
 
 gettextize --copy --force < /dev/null
-
-# patch for libglade strings
-cat >> po/Makefile.in.in << "EOF"
-
-# Added by autogen.sh
-../po/glade-files.c: ../pixmaps/ogle_gui.glade
-	libglade-xgettext -o $@ $<
-POFILES: glade-files.c
-EOF
-
 aclocal
 libtoolize --copy --automake
 automake --copy --add-missing
