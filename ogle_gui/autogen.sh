ac1fbe82598725b579d392e00dd5387f82b5b202
d95mback <d95mback>
2001-11-29 21:00:52 +0000

    fix for gettext/libglade

--- ogle_gui/autogen.sh
+++ ogle_gui/autogen.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 set -x
 
-gettextize
+gettextize --copy --force < /dev/null
+
+# patch for libglade strings
+cat >> po/Makefile.in.in << "EOF"
+
+# Added by autogen.sh
+../po/glade-files.c: ../ogle_gui.glade
+	libglade-xgettext -o $@ $<
+POFILES: glade-files.c
+EOF
+
 aclocal
 libtoolize --copy --automake
 automake --copy --add-missing
