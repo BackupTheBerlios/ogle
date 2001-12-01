ddb4c1891d95d79432465b8fcdbc80ea410379cb
d95mback <d95mback>
2001-12-01 17:53:44 +0000

    now compiles without nls if needed

--- ogle_gui/src/myintl.h
+++ ogle_gui/src/myintl.h
@@ -1,8 +1,13 @@
 #ifndef MYINTL_H
 #define MYINTL_H
 
+#ifdef ENABLE_NLS
 #include <libintl.h>
 #define _(x) (gettext(x))
+#else
+#define _(x) (x)
+#endif
+
 #define N_(x) (x)
 
 #endif /* MYINTL_H */
