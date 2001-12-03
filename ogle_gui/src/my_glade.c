01a6e41fd1d4fb1135cec8b44435aa633d2d6c6f
d95mback <d95mback>
2001-12-03 22:19:27 +0000

    Wasn't quite done

--- ogle_gui/src/my_glade.c
+++ ogle_gui/src/my_glade.c
@@ -2,6 +2,7 @@
 #  include <config.h>
 #endif
 
+#include <stdio.h>
 #include <dlfcn.h>
 #include <gtk/gtk.h>
 #include <glade/glade.h>
@@ -10,6 +11,7 @@
 
 // my own versions of the glade calls
 static void (*my_glade_init)(void);
+static GladeXML * (*my_glade_xml_new)(const char *, const char *);
 static void (*my_glade_xml_signal_autoconnect)(GladeXML *);
 static GtkWidget * (*my_glade_xml_get_widget)(GladeXML *, const char *);
 
@@ -21,20 +23,25 @@ void my_glade_setup (const char *filename)
 {
   void *glade_lib;
 
+  fprintf(stderr, "calling dlopen(" LIBGLADE_LIB ")\n");
+
   glade_lib = dlopen (LIBGLADE_LIB, RTLD_NOW);
 
+  fprintf(stderr, "glade_lib = %lx\n", glade_lib);
+
   my_glade_init = dlsym(glade_lib, "glade_init");
+  my_glade_xml_new = dlsym(glade_lib, "glade_xml_new");
   my_glade_xml_signal_autoconnect = 
     dlsym(glade_lib, "glade_xml_signal_autoconnect");
   my_glade_xml_get_widget =
     dlsym(glade_lib, "glade_xml_get_widget");
 
-  (*my_glade_init)();
-  xml = (*glade_xml_new)(filename, NULL);
-  (*glade_xml_signal_autoconnect)(xml);
+  my_glade_init();
+  xml = my_glade_xml_new(filename, NULL);
+  my_glade_xml_signal_autoconnect(xml);
 }
 
 GtkWidget *get_glade_widget(const char *name)
 {
-  return (*my_glade_xml_get_widget)(xml, name);
+  return my_glade_xml_get_widget(xml, name);
 }
