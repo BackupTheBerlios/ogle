ba4a6caf788aeada9b905ec661685c5b9b0d1a95
d95mback <d95mback>
2001-12-11 23:15:31 +0000

    Now updates when attributes are change from outside

--- xvattr/gxvattr.c
+++ xvattr/gxvattr.c
@@ -29,17 +29,38 @@
 #include <gtk/gtk.h>
 #include <gdk/gdkx.h>
 
+// This is set when changing widget values from event handler, so that
+// no new events are generated
+static gboolean changing;
+
+// Could be replaced by a more efficient data structure, but...come on...
+// anyway, this is a list of all the different properties
+static GList *properties;
+
 struct property_info {
-  XvPortID port;
-  Atom     atom;
-  int      standard;
+  XvPortID  port;
+  Atom      atom;
+  GtkObject *controller;
+  void      (*set_widget)(GtkObject *, long);
+  int       default_value;
 };
 
-static void set_port_attribute (GtkAdjustment *adjustment, 
+static void set_adjustment_attribute (GtkAdjustment *adjustment, 
                                struct property_info *property)
 {
-  XvSetPortAttribute(GDK_DISPLAY (), property->port, property->atom, adjustment->value);
-  XFlush(GDK_DISPLAY ());
+  if(!changing) {
+    XvSetPortAttribute(
+        GDK_DISPLAY (), 
+        property->port, 
+        property->atom, 
+        adjustment->value);
+    XFlush(GDK_DISPLAY ());
+  }
+}
+
+static void set_adjustment_widget (GtkObject *adjustment, long value)
+{
+  gtk_adjustment_set_value(GTK_ADJUSTMENT(adjustment), value);
 }
 
 static void set_color_attribute (GtkColorSelection *color_selection,
@@ -48,38 +69,98 @@ static void set_color_attribute (GtkColorSelection *color_selection,
   gdouble color[4];
   int color_value;
 
-  gtk_color_selection_get_color(GTK_COLOR_SELECTION(color_selection), color);
-  color_value = (((int)(color[0]*255)) << 16) + 
-                (((int)(color[1]*255)) << 8) + 
-		(((int)(color[2]*255)));
-  XvSetPortAttribute(GDK_DISPLAY (), property->port, property->atom, color_value);
-  XFlush(GDK_DISPLAY ());
+  if(!changing) {
+    gtk_color_selection_get_color(GTK_COLOR_SELECTION(color_selection), color);
+    color_value = 
+      (((int)(color[0]*255)) << 16) + 
+      (((int)(color[1]*255)) << 8) + 
+      (((int)(color[2]*255)));
+    XvSetPortAttribute(
+        GDK_DISPLAY (), 
+        property->port, 
+        property->atom, 
+        color_value);
+    XFlush(GDK_DISPLAY ());
+  }
 }
 
-static void set_bool_attribute (GtkToggleButton *toggle, 
+static void set_color_widget (GtkObject *color_selection, long value)
+{
+  double colors[4] = {
+    (value >> 16)         / 255.0, 
+    ((value >> 8) & 0xff) / 255.0,
+    (value        & 0xff) / 255.0,
+    0
+  };
+  gtk_color_selection_set_color(
+      GTK_COLOR_SELECTION(color_selection),
+      colors);
+}
+
+
+static void set_bool_attribute (GtkWidget *toggle, 
                                 struct property_info *property)
 {
   gboolean active;
 
-  active = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(toggle));
-  XvSetPortAttribute(GDK_DISPLAY (), property->port, property->atom, active?1:0);
-  XFlush(GDK_DISPLAY ());
+  if(!changing) {
+    active = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(toggle));
+    XvSetPortAttribute(
+        GDK_DISPLAY (), 
+        property->port, 
+        property->atom, 
+        active?1:0);
+    XFlush(GDK_DISPLAY ());
+  }
+}
+
+static void set_bool_widget (GtkObject *toggle, long value)
+{
+  gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(toggle), value);
 }
 
 static void set_only_attribute (GtkWidget *widget, 
                                 struct property_info *property)
 {
-  XvSetPortAttribute(GDK_DISPLAY (), property->port, property->atom, property->standard);
-  XFlush(GDK_DISPLAY ());
+  if(!changing) {
+    XvSetPortAttribute(GDK_DISPLAY (), property->port, property->atom, property->default_value);
+    XFlush(GDK_DISPLAY ());
+  }
 }
 
 static GdkFilterReturn
-xv_filter (XEvent *xevent, GdkEvent *event, gpointer user_data)
+xv_filter (GdkXEvent *gdk_xevent, GdkEvent *event, gpointer user_data)
 {
-	if (xevent->type == XvPortNotify)
-		g_print ("woo, something happened: %d!\n", xevent->type);
+  XEvent *xevent = (XEvent *) gdk_xevent;
+  int xv_event_base = GPOINTER_TO_INT(user_data);
 
-	return GDK_FILTER_CONTINUE;
+  
+  // if this is a port notify event, call the right function to update widget
+  if (xevent->type == xv_event_base + XvPortNotify) {
+    XvPortNotifyEvent *ev = (XvPortNotifyEvent *) xevent;
+    GList *i;
+
+    changing = TRUE;
+
+    // find the property to go with this event
+    for(i = properties; i != NULL; i = g_list_next(i)) {
+      struct property_info *p = (struct property_info *) (i->data);
+      if (p->port == ev->port_id &&
+          p->atom == ev->attribute) {
+        if (p->set_widget != NULL) {
+          p->set_widget(p->controller, ev->value);
+        }
+        break;
+      }
+    }
+
+    changing = FALSE;
+
+    return GDK_FILTER_REMOVE;
+  }
+
+
+  return GDK_FILTER_CONTINUE;
 }
 
 int main(int argc, char **argv)
@@ -93,21 +174,31 @@ int main(int argc, char **argv)
   GtkWidget *win;
   GtkWidget *notebook;
 
+  changing = FALSE;
+
   gtk_init(&argc, &argv);
 
-  if(XvQueryExtension(GDK_DISPLAY (), &xv_version, &xv_release, &xv_request_base,
-		      &xv_event_base, &xv_error_base) != Success) {
+  if(XvQueryExtension(
+        GDK_DISPLAY (), 
+        &xv_version, 
+        &xv_release, 
+        &xv_request_base,
+        &xv_event_base, 
+        &xv_error_base
+        ) != Success) {
     fprintf(stderr, "Xv not found\n");
     exit(1);
   }
 
-  XvQueryAdaptors(GDK_DISPLAY (), DefaultRootWindow(GDK_DISPLAY ()),
-		  &num_adaptors, &adaptor_info);  
-  
+  XvQueryAdaptors(
+      GDK_DISPLAY (), 
+      DefaultRootWindow(GDK_DISPLAY ()),
+      &num_adaptors, 
+      &adaptor_info);  
   
   gtk_init(&argc, &argv);
 
-  gdk_window_add_filter (NULL, xv_filter, NULL);
+  gdk_window_add_filter (NULL, xv_filter, GINT_TO_POINTER(xv_event_base));
   
   win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   
@@ -118,7 +209,7 @@ int main(int argc, char **argv)
   for(n = 0; n < num_adaptors; n++) {
     int base_id;
     int num_ports;
-    int m;
+    int port;
 
     GtkWidget *vbox;
     GtkWidget *adaptor_name;
@@ -130,17 +221,17 @@ int main(int argc, char **argv)
     adaptor_name = gtk_label_new(adaptor_info[n].name);
     gtk_notebook_append_page(GTK_NOTEBOOK(notebook), vbox, adaptor_name);
 
-    for(m = base_id; m < base_id+num_ports; m++) {
+    for(port = base_id; port < base_id+num_ports; port++) {
       int num;
       int k;
       int cur_val = 0;
       Atom val_atom;
       XvAttribute *xvattr;
 
-      // this should be used to get events, but gtk won't see them anyway
-      XvSelectPortNotify(GDK_DISPLAY (), m, True);
+      // Get events when this port changes
+      XvSelectPortNotify(GDK_DISPLAY (), port, True);
 
-      xvattr = XvQueryPortAttributes(GDK_DISPLAY (), m, &num);
+      xvattr = XvQueryPortAttributes(GDK_DISPLAY (), port, &num);
       for(k = 0; k < num; k++) {
         GtkWidget *hbox;
         GtkWidget *label;
@@ -150,26 +241,24 @@ int main(int argc, char **argv)
         hbox = gtk_hbox_new(FALSE, 0);
         val_atom = XInternAtom(GDK_DISPLAY (), xvattr[k].name, False);
         if(xvattr[k].flags & XvGettable) {
-          if(XvGetPortAttribute(GDK_DISPLAY (), m, val_atom, &cur_val) != Success) {
-            fprintf(stderr, "Couldn't get attribute\n");
-            exit(1);
-          }
+          XvGetPortAttribute(
+              GDK_DISPLAY (), 
+              port, 
+              val_atom, 
+              &cur_val);
         }
 
 	property = malloc(sizeof (struct property_info));
-	property->port = m;
+	property->port = port;
 	property->atom = val_atom;
-	property->standard = xvattr[k].min_value;
+	property->default_value = xvattr[k].min_value;
 
         label = gtk_label_new (xvattr[k].name);
         if (!strcmp(xvattr[k].name, "XV_COLORKEY")) {
-          double colors[4] = {(cur_val >> 16)         / 255.0, 
-                              ((cur_val >> 8) & 0xff) / 255.0,
-                              (cur_val        & 0xff) / 255.0,
-	                      0};
           manipulator = gtk_color_selection_new();
-          gtk_color_selection_set_color(GTK_COLOR_SELECTION(manipulator),
-                                        colors);
+          set_color_widget(manipulator, cur_val);
+          property->controller = (GtkObject *) manipulator;
+          property->set_widget = set_color_widget;
           if (xvattr[k].flags & XvSettable) {
 	    gtk_signal_connect(GTK_OBJECT(manipulator), "color-changed",
 		               GTK_SIGNAL_FUNC(set_color_attribute), property);
@@ -180,7 +269,9 @@ int main(int argc, char **argv)
 	           xvattr[k].max_value == 1) {
 	  // boolean value, use check button
 	  manipulator = gtk_check_button_new();
-	  gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(manipulator),cur_val);
+	  set_bool_widget(manipulator, cur_val);
+          property->controller = (GtkObject *) manipulator;
+          property->set_widget = set_bool_widget;
           if (xvattr[k].flags & XvSettable) {
 	    gtk_signal_connect(GTK_OBJECT(manipulator), "toggled",
 		               GTK_SIGNAL_FUNC(set_bool_attribute), property);
@@ -191,6 +282,7 @@ int main(int argc, char **argv)
 	           xvattr[k].min_value == xvattr[k].max_value) {
 	  // only settable, and one value
 	  manipulator = gtk_button_new_with_label("Set");
+          property->set_widget = NULL;
           if (xvattr[k].flags & XvSettable) {
 	    gtk_signal_connect(GTK_OBJECT(manipulator), "clicked",
 		               GTK_SIGNAL_FUNC(set_only_attribute), property);
@@ -206,17 +298,23 @@ int main(int argc, char **argv)
                                            1,
                                            10,
                                            0);
+          property->controller = adjustment;
+          property->set_widget = set_adjustment_widget;
+
           manipulator = gtk_hscale_new (GTK_ADJUSTMENT(adjustment));
           gtk_scale_set_digits(GTK_SCALE(manipulator), 0);
 
           // enable the manipulator if settable, disable it otherwise
           if (xvattr[k].flags & XvSettable) {
             gtk_signal_connect (GTK_OBJECT(adjustment), "value-changed",
-                                GTK_SIGNAL_FUNC(set_port_attribute), property);
+                                GTK_SIGNAL_FUNC(set_adjustment_attribute), property);
           } else {
             gtk_widget_set_sensitive(GTK_WIDGET(manipulator), FALSE);
           }
         } 
+
+        // add to list of properties
+        properties = g_list_append(properties, property);
 
         gtk_box_pack_start(GTK_BOX(hbox), 
                           label,
