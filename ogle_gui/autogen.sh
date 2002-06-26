3709426549cb38b0c93be3e0c686367328d26900
d95mback <d95mback>
2002-06-26 15:19:08 +0000

    Use included gettext macros

--- ogle_gui/autogen.sh
+++ ogle_gui/autogen.sh
@@ -3,7 +3,7 @@ set -x
 
 # Don't run gettextize automatically, you shouldn't really
 #gettextize --copy --force < /dev/null
-aclocal
+aclocal -I m4
 libtoolize --copy --automake
 automake --copy --add-missing
 autoconf
