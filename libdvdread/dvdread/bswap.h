c5078341b819dcafd6e5cd417d41217b7c4e2e00
bjorn <bjorn>
2005-09-19 14:14:34 +0000

    DragonFly patch from Joerg Sonnenberger

--- libdvdread/dvdread/bswap.h
+++ libdvdread/dvdread/bswap.h
@@ -70,13 +70,19 @@
        (((x) & 0x00000000000000ff) << 56))
 #endif /* _FreeBSD_version >= 500000 */
 
+#elif defined(__DragonFly__)
+#include <sys/endian.h>
+#define B2N_16(x) x = be16toh(x)
+#define B2N_32(x) x = be32toh(x)
+#define B2N_64(x) x = be64toh(x)
+
 /* This is a slow but portable implementation, it has multiple evaluation 
  * problems so beware.
  * Old FreeBSD's and Solaris don't have <byteswap.h> or any other such 
  * functionality! 
  */
 
-#elif defined(__FreeBSD__) || defined(__sun) || defined(__bsdi__) || defined(WIN32) || defined(__BEOS__)
+#elif defined(__FreeBSD__) || defined(__sun) || defined(__bsdi__) || defined(WIN32) || defined(__BEOS__) || defined(__INTERIX)
 #define B2N_16(x)                               \
   x = ((((x) & 0xff00) >> 8) |                  \
        (((x) & 0x00ff) << 8))
